ff320c197e4f108d57d10ada27e9e85c72ee456b
Hyeeeein <khi_9866@naver.com>
2023-01-17 16:07:34 +0900

    fix: conflict 해결

--- src/api/requests.tsx
+++ src/api/requests.tsx
@@ -12,53 +12,53 @@
 import axios from './axios';
 
 // 검색어로 인한 비디오 데이터
-export const getSearchData = async (title) => {
+export const getSearchData = async (title: string) => {
   try {
     const response = await axios.get(`/search?part=snippet&maxResults=10&q=${title}`);
     return response;
   } catch (error) {
-    console.error(error.message);
+    // console.error(error.message);
   }
 };
 
 // 특정 비디오 데이터
-export const getVideoDetail = async (videoId) => {
+export const getVideoDetail = async (videoId: string) => {
   try {
     const response = await axios.get(
       `/videos?part=snippet&part=contentDetails&part=player&part=statistics&id=${videoId}`,
     );
     return response;
   } catch (error) {
-    console.error(error.message);
+    // console.error(error.message);
   }
 };
 
 // 특정 채널 정보 데이터
-export const getChannelInfo = async (channelId) => {
+export const getChannelInfo = async (channelId: string) => {
   try {
     const response = await axios.get(`/channels?part=snippet&part=statistics&part=contentDetails&id=${channelId}`);
     return response;
   } catch (error) {
-    console.error(error.message);
+    // console.error(error.message);
   }
 };
 
 // 댓글 데이터
-export const getComment = async (videoId) => {
+export const getComment = async (videoId: string) => {
   try {
     const response = await axios.get(`/commentThreads?part=snippet&videoId=${videoId}`);
     return response;
   } catch (error) {
-    console.error(error.message);
+    // console.error(error.message);
   }
 };
 
 // 관련 비디오 데이터
-export const getRelationVideo = async (videoId) => {
+export const getRelationVideo = async (videoId: string) => {
   try {
     const response = await axios.get(`/search?part=snippet&maxResults=10&relatedToVideoId=${videoId}&type=video`);
     return response;
   } catch (error) {
-    console.error(error.message);
+    // console.error(error.message);
   }
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,13 @@
 import React, { useEffect } from 'react';
-import axios from '../api/axios';
-import { getSearchData, getVideoDetail, getChannelInfo, getComment, getRelationVideo } from './../api/requests';
+// import { getSearchData, getVideoDetail, getChannelInfo, getComment, getRelationVideo } from './../api/requests';
 
 export default function Header() {
+  // 예시
+  // channelId : "UCPydsWBQpnXGICE-XWGNdow"
+  // videoId : "b1QpN8cJQCo"
   useEffect(() => {
     // getRelationVideo('b1QpN8cJQCo').then((data) => console.log(data));
   });
 
   return <div>Header</div>;
 }
-
-// channelId : "UCPydsWBQpnXGICE-XWGNdow"
-// videoId : "b1QpN8cJQCo"
