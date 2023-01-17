b71c9d07b8b21907c17f44a6889fa2e565ea61ed
hyorimm <hyorimm@gmail.com>
2023-01-17 23:12:10 +0900

    #13 - feat: 메인페이지 마크업 진행 중

--- src/components/VideoCard/VideoCard.tsx
+++ src/components/VideoCard/VideoCard.tsx
@@ -1,31 +1,65 @@
-import React from "react";
-import "./VideoCard.scss";
-import { FiMoreVertical } from "react-icons";
+import React, { useEffect, useState } from 'react';
+import './VideoCard.scss';
+import { FiMoreVertical } from 'react-icons/fi';
+import { format, render, cancel, register } from 'timeago.js';
+import axios from 'axios';
 
 export default function VideoCard() {
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = (await axios.get('./videos/popular.json')).data.items;
+      console.log('res', res);
+      setDatas(res);
+    };
+    getData();
+  }, []);
+
+  // // format timestamp
+  // format(1544666010224);
+  // // format date instance
+  // format(new Date(1544666010224));
+  // // format date string
+  // format('2018-12-12');
+  // // format with locale
+  // format(1544666010224, 'zh_CN');
+  // // format with locale and relative date
+  // format(1544666010224, 'zh_CN', '2018-11-11');
+  // // e.g.
+  // format(Date.now() - 11 * 1000 * 60 * 60); // returns '11 hours ago'
+
   return (
-    <div className="videoCard">
-      <div className="videoPreview">
-        PreviewImg
-        <div className="videoDuration">
-          <span>00:00</span>
-        </div>
-      </div>
-      <div className="videoInfoContainer">
-        <div className="avatarContainer">
-          <img src="https://cdn.pixabay.com/photo/2016/07/03/18/36/youtube-1495277_960_720.png" alt="" />
-        </div>
-        <div className="videoTextContainer">
-          <h3>videoTitle</h3>
-          <div className="videoInfo">
-            <div className="channelName">channelname</div>
-            <div className="metatag">metatag</div>
+    <>
+      {datas.map((data) => (
+        <div className="videoCard" key={data.id}>
+          <div className="videoPreview">
+            <img src={data.snippet.thumbnails.high.url} alt={data.snippet.title} />
+            <div className="videoDuration">
+              <span>{data.contentDetails.duration}</span>
+            </div>
+          </div>
+          <div className="videoInfoContainer">
+            <div className="avatarContainer">
+              <img src="https://cdn.pixabay.com/photo/2016/07/03/18/36/youtube-1495277_960_720.png" alt="" />
+            </div>
+            <div className="videoTextContainer">
+              <h4>{data.snippet.title}</h4>
+              <div className="videoInfo">
+                <div className="channelName">{data.snippet.channelTitle}</div>
+                <div className="metaTag">
+                  <span>{data.statistics.likeCount} views</span>
+                  <span className="dot_separator"> • </span>
+                  <span>{}</span>
+                </div>
+              </div>
+            </div>
+            <div className="dotContainer">
+              <FiMoreVertical />
+            </div>
           </div>
         </div>
-        <div className="dotContainer">
-          <FiMoreVertical />
-        </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 }
