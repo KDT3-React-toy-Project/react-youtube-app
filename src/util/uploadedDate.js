afb703d68a932d11789c7de2cd6275a92f4ad5c5
hyerimhan <hnhyerim@gmail.com>
2023-01-18 20:04:24 +0900

    fix: modified VidoCard & modified viewCount, uploadedDate

---
+++ src/util/uploadedDate.js
@@ -0,0 +1,8 @@
+import { register, format } from "timeago.js"
+import koLocale from "timeago.js/lib/lang/ko";
+
+register('ko', koLocale);
+
+export function uploadedDate(date, lang = "ko") {
+  return format(date, lang)
+}
