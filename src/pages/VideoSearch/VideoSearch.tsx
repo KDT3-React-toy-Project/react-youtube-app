5c00c835a621ad02b6166e64504c56c9f20d324c
0nesan <0nesan.o8o@gmail.com>
2023-01-20 07:44:35 +0900

    feat:검색 페이지

--- src/pages/VideoSearch/VideoSearch.tsx
+++ src/pages/VideoSearch/VideoSearch.tsx
@@ -1,50 +1,57 @@
 import axios from 'axios';
-import { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { getSearchData } from 'src/api/requests';
+import { useEffect, useState, useContext } from 'react';
+import { useParams, Link } from 'react-router-dom';
+// import { getSearchData, getVideoDetail } from 'src/api/requests';
 import { ShowContext } from 'src/contexts/store';
+import './VideoSearch.scss'
 
 function VideoSearch() {
   const [shownav] = useContext(ShowContext);
   const [data, setData] = useState<any>([]);
-  const ytDurationFormat = require('youtube-duration-format');
   const { keyword } = useParams();
-
+  
   useEffect(() => {
-    const getData = async () => {
-      const res = await getSearchData(keyword);
+    // const getData = async () => {
+    //   const res = await getSearchData(keyword);
+    //   setData(res.data.items);
+    //   console.log(res.data);
+    // };
+    // getData();
+    const getSearchData = async () => {
+      const res = await axios.get(`/videos/search.json`);
       setData(res.data.items);
-      console.log(res.data)
-    }
-    getData();
+      console.log(res);
+    };
+    getSearchData();
   }, [keyword]);
 
-  console.log(data);
   return (
-    <div className='search-wrap'>
-      {
-        data.map((item : any, index : number) => {
+    <div className="search-container" nav-check={shownav ? 'show' : 'none'}>
+      <div className="search-wrap">
+        {data.map((item: any, index: number) => {
           return (
-            <div key = {index}>
-              <div>
-                <img src={item.snippet.thumbnails.high.url} alt={item.snippet.title} />
-                <p>영상 길이</p>
+            <Link to={`/videos/watch/${item.id.videoId}`} key={index}>
+              <div className="search-video-list">
+                <div className="search-thumbnails">
+                    <img src={item.snippet.thumbnails.high.url} alt={item.snippet.title} />
+                    <p></p>
+                </div>
+                <div className='search-video-info'>
+                  <p className='search-video-title'>{item.snippet.title}</p>
+                  <p>날짜, 조회수</p>
+                  <p className='search-channel-title'>
+                    <span><img src={''} alt="" /></span>
+                    <span>{item.snippet.channelTitle}</span>
+                  </p>
+                  <p className="search-video-description">{item.snippet.description}</p>
+                </div>
               </div>
-              <div>
-                <p>{item.snippet.title}</p>
-                <p>날짜, 조회수</p>
-                <p>
-                  <span></span>
-                  <span>{item.snippet.channelTitle}</span>
-                </p>
-                <p>{item.snippet.description}</p>
-              </div>
-            </div>
+            </Link>
           );
-        })
-      }
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
 export default VideoSearch;
