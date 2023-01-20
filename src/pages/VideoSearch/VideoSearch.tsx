3d75572698c7732806ed6c5ff4251b5670f9da42
0nesan <0nesan.o8o@gmail.com>
2023-01-20 16:52:17 +0900

    feat : 검색 페이지 영상길이 제외 작업완료

--- src/pages/VideoSearch/VideoSearch.tsx
+++ src/pages/VideoSearch/VideoSearch.tsx
@@ -1,9 +1,9 @@
-import axios from 'axios';
 import { useEffect, useState, useContext } from 'react';
 import { useParams, Link } from 'react-router-dom';
-// import { getSearchData, getVideoDetail } from 'src/api/requests';
+import { getSearchData } from 'src/api/requests';
 import { ShowContext } from 'src/contexts/store';
 import './VideoSearch.scss';
+import VideoSearchInfo from './VideoSearchInfo/VideoSearchInfo';
 
 function VideoSearch() {
   const [shownav] = useContext(ShowContext);
@@ -11,18 +11,18 @@ function VideoSearch() {
   const { keyword } = useParams();
 
   useEffect(() => {
-    // const getData = async () => {
-    //   const res = await getSearchData(keyword);
-    //   setData(res.data.items);
-    //   console.log(res.data);
-    // };
-    // getData();
-    const getSearchData = async () => {
-      const res = await axios.get(`/videos/search.json`);
+    const getData = async () => {
+      const res = await getSearchData(keyword);
       setData(res.data.items);
-      console.log(res);
+      console.log(res.data);
     };
-    getSearchData();
+    getData();
+    // const getSearchData = async () => {
+    //   const res = await axios.get(`/videos/search.json`);
+    //   setData(res.data.items);
+    //   console.log(res);
+    // };
+    // getSearchData();
   }, [keyword]);
 
   return (
@@ -30,23 +30,13 @@ function VideoSearch() {
       <div className="search-wrap">
         {data.map((item: any, index: number) => {
           return (
-            <Link to={`/videos/watch/${item.id.videoId}`} key={index}>
+            <Link to={`/videos/watch/${item.id.videoId}`} key={index} state={{ channelId: item.snippet.channelId }}>
               <div className="search-video-list">
                 <div className="search-thumbnails">
                   <img src={item.snippet.thumbnails.high.url} alt={item.snippet.title} />
                   <p></p>
                 </div>
-                <div className="search-video-info">
-                  <p className="search-video-title">{item.snippet.title}</p>
-                  <p>날짜, 조회수</p>
-                  <p className="search-channel-title">
-                    <span>
-                      <img src={''} alt="" />
-                    </span>
-                    <span>{item.snippet.channelTitle}</span>
-                  </p>
-                  <p className="search-video-description">{item.snippet.description}</p>
-                </div>
+                <VideoSearchInfo videoId={item.id.videoId}  channelId={item.snippet.channelId}/>
               </div>
             </Link>
           );
