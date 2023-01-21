28ddd2efb8fef7e5e2b4180b0d6f6b57c786842d
team7 <hnhyerim@gmail.com>
2023-01-21 14:00:02 +0900

    fix: deploy conflict resolved

--- src/pages/VideoDetail/VideoDetail.tsx
+++ src/pages/VideoDetail/VideoDetail.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable unused-imports/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { getComment, getVideoDetail } from 'src/api/requests';
